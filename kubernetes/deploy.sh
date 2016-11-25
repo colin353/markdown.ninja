daae0d406409240156132eeeb96b93e4343d15bc
Colin Merkel <colin353@gmail.com>
2016-11-24 19:50:06 -0500

    kubectl

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 export GOOGLE_APPLICATION_CREDENTIALS=~/account-auth.json
 
-sudo chmod 777 /home/ubuntu/.kube/config
+sudo chmod -R 777 /home/ubuntu/.kube
 
 # Update Kubernetes replicationController
 envsubst < kubernetes/portfolio.rc.yaml | kubectl create -f -
